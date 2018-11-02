8d062aac31f6735fe5b194274c792f35af8b5bb9
RuizAlmeydaRonaldJavier <ruizalmeyda.ronaldjavier@gmail.com>
2018-11-01 21:47:19 -0500

    Depuracion

--- PROYECTO/DAO/aulaDAO.php
+++ PROYECTO/DAO/aulaDAO.php
@@ -1,6 +1,6 @@
 <?php
-require_once('../DAL/DBAccess.php');
-require_once('../BOL/aula.php');
+require_once '../DAL/DBAccess.php';
+require_once '../BOL/aula.php';
 
 class AulaDAO
 {
@@ -16,15 +16,15 @@ class AulaDAO
 	{
 		try
 		{
-		$statement = $this->pdo->prepare("CALL up_registrar_aula(?,?,?,?,?,?,?)");
-		$statement->bindParam(1, $aula->__GET('descripcion'));
-		$statement->bindParam(2, $aula->__GET('numero_aula'));
-		$statement->bindParam(3, $aula->__GET('numero_alumno'));
-		$statement->bindParam(4, $aula->__GET('turno'));
-		$statement->bindParam(5, $aula->__GET('id_docente'));
-	  $statement->bindParam(6, $aula->__GET('id_grado'));
-		$statement->bindParam(7, $aula->__GET('id_seccion'));
-    $statement -> execute();
+			$statement = $this->pdo->prepare("CALL up_registrar_aula(?,?,?,?,?,?,?)");
+			$statement->bindParam(1, $aula->__GET('descripcion'));
+			$statement->bindParam(2, $aula->__GET('numero_aula'));
+			$statement->bindParam(3, $aula->__GET('numero_alumno'));
+			$statement->bindParam(4, $aula->__GET('turno'));
+			$statement->bindParam(5, $aula->__GET('id_docente'));
+		  $statement->bindParam(6, $aula->__GET('id_grado'));
+			$statement->bindParam(7, $aula->__GET('id_seccion'));
+	    $statement -> execute();
 		} catch (Exception $e)
 		{
 			die($e->getMessage());
@@ -36,26 +36,25 @@ class AulaDAO
 		try
 		{
 			$result = array();
-
 			$statement = $this->pdo->prepare("call up_listar_aula()");
 			$statement->execute();
 
 			foreach($statement->fetchAll(PDO::FETCH_OBJ) as $r)
 			{
-				$aul = new Aula();
+				$aula = new Aula();
 
-		    $aul->__SET('id_aula', $r->id_aula);
-				$aul->__SET('descripcion', $r->descripcion);
-				$aul->__SET('numero_aula', $r->numero_aula);
-				$aul->__SET('numero_alumno', $r->numero_alumno);
-        $aul->__SET('turno', $r->turno);
-				$aul->__GET('id_docente')->__GET('id_persona')->__SET('nombres', $r->nombre);
-				$aul->__GET('id_docente')->__GET('id_persona')->__SET('apellidosP', $r->apellido_paterno);
-				$aul->__GET('id_docente')->__GET('id_persona')->__SET('apellidosM', $r->apellido_materno);
-				$aul->__GET('id_grado')->__SET('grado', $r->grado);
-				$aul->__GET('id_seccion')->__SET('seccion',$r->seccion);
+		    $aula->__SET('id_aula', $r->id_aula);
+				$aula->__SET('descripcion', $r->descripcion);
+				$aula->__SET('numero_aula', $r->numero_aula);
+				$aula->__SET('numero_alumno', $r->numero_alumno);
+        $aula->__SET('turno', $r->turno);
+				$aula->__GET('id_docente')->__GET('id_persona')->__SET('nombres', $r->nombre);
+				$aula->__GET('id_docente')->__GET('id_persona')->__SET('apellidosP', $r->apellido_paterno);
+				$aula->__GET('id_docente')->__GET('id_persona')->__SET('apellidosM', $r->apellido_materno);
+				$aula->__GET('id_grado')->__SET('grado', $r->grado);
+				$aula->__GET('id_seccion')->__SET('seccion',$r->seccion);
 
-				$result[] = $aul;
+				$result[] = $aula;
 			}
 
 			return $result;
