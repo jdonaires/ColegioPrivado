fb98e64508b6970ae940e70d463582ea31232d4a
farojastasayco <fabiolarojastasayco@gmail.com>
2018-11-01 20:32:09 -0500

    Datos de la Tabla funcion
    Ingreso de valores de datos de la Tabla funcion

--- PROYECTO/DB/dato.sql
+++ PROYECTO/DB/dato.sql
@@ -4,6 +4,10 @@ INSERT INTO `tipos_documentos` (`id_tdocumento`, `tipo_documento`) VALUES ('2', 
 INSERT INTO `estados_civiles` (`id_ecivil`, `estado_civil`) VALUES ('1', 'Soltero');
 INSERT INTO `estados_civiles` (`id_ecivil`, `estado_civil`) VALUES ('2', 'Casado');
 
+INSERT INTO `funciones` (`id_funcion`, `funcion`) VALUES ('1', 'Sin funciones extras');
+INSERT INTO `funciones` (`id_funcion`, `funcion`) VALUES ('2', 'Puede Matricular');
+
+
 INSERT INTO grados VALUES (null, 'Primer Grado'), (null, 'Segundo Grado');
 INSERT INTO secciones VALUES (null, 'A'), (null, 'B'), (null, 'C');
 
