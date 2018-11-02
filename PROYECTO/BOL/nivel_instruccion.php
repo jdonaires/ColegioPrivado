8e033ed00b6723d8bc803a510927d75b0c46d416
kevinhurtado98 <44491747+kevinhurtado98@users.noreply.github.com>
2018-11-01 20:21:45 -0500

    Creacion nivel_instruccion BOL y DAO

---
+++ PROYECTO/BOL/nivel_instruccion.php
@@ -0,0 +1,18 @@
+<?php
+class Nivel_instruccion
+{
+	private $id_ninstruccion;
+	private $nivel_instruccion;
+
+
+	public function __GET($x)
+	{
+		return $this->$x;
+	}
+	public function __SET($x, $y)
+	{
+		return $this->$x = $y;
+	}
+	
+}
+?>
