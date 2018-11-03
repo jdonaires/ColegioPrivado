59c098a55a21edbd0bd89ba1b79efaa59c2b650c
DeyaniraCancharisT <44514607+DeyaniraCancharisT@users.noreply.github.com>
2018-11-03 11:04:57 -0500

    estado civilDAO

---
+++ PROYECTO/DAO/estado_civilDAO.php
@@ -0,0 +1,59 @@
+<?php
+require_once '../DAL/DBAccess.php';
+require_once '../BOL/estado_civil.php';
+
+class Estado_civilDAO
+{
+	private $pdo;
+
+	public function __CONSTRUCT()
+	{
+			$dba = new DBAccess();
+			$this->pdo = $dba->get_connection();
+	}
+
+	public function registrar(Estado_civil $estado_civil)
+	{
+		try
+		{
+		$statement = $this->pdo->prepare("CALL up_registrar_estado_civil(?)");
+    $statement->bindParam(1,$estado_civil)
+		->__GET('estado_civil'));
+    $statement -> execute();
+
+		} catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function buscar(Estado_civil $estado_civil)
+	{
+		try
+		{
+			$result = array();
+
+			$statement = $this->pdo->prepare("call up_buscar_estado_civil(?)");
+			$statement->bindParam(1,$estado_civil->__GET('estado_civil'));
+			$statement->execute();
+
+			foreach($statement->fetchAll(PDO::FETCH_OBJ) as $r)
+			{
+				$estado_civil = new Estado_civil();
+
+				$estado_civil->__SET('id', $r->id_);
+
+
+				$result[] = $estado_civil;
+			}
+
+			return $result;
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+}
+
+?>
