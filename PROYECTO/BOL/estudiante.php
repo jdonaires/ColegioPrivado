59af60f2e081d20ea9b24f41ea1fff95a8c53813
AlexandreTMz <alexandre99.v@gmail.com>
2018-11-03 20:59:53 -0500

    Clase estudiante
    Clase estudiante y archivo DAO

---
+++ PROYECTO/BOL/estudiante.php
@@ -0,0 +1,26 @@
+<?php 
+require_once 'persona.php';
+
+class Estudiante
+{
+	private $id_persona;
+	private $codigo_estudiante;
+
+	// Constructor
+	public function __construct(){
+		$this->id_persona = new Persona();
+	}
+
+	public function __GET($x)
+	{ 
+		return $this->$x; 
+	}
+	public function __SET($x, $y)
+	{ 
+		return $this->$x = $y; 
+	}
+
+}
+
+
+?>
---
+++ PROYECTO/DAO/estudianteDAO.php
@@ -0,0 +1,61 @@
+<?php
+require_once('../DAL/DBAccess.php');
+require_once('../BOL/estudiante.php');
+
+class EstudianteDAO
+{
+	private $pdo;
+
+	public function __construct()
+	{
+			$dba = new DBAccess();
+			$this->pdo = $dba->get_connection();
+	}
+
+	public function Registrar(Estudiante $estudiante)
+	{
+		try
+		{
+		$statement = $this->pdo->prepare("CALL up_registrar_Estudiante(?,?)");
+        $statement->bindParam(1,$estudiante->__GET('id_persona')->__GET('id_persona'));
+		$statement->bindParam(2,$estudiante->__GET('codigo_estudiante'));
+        $statement->execute();
+
+		} 
+			catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Listar(Estudiante $estudiante)
+	{
+		try
+		{
+			$result = array();
+
+			$statement = $this->pdo->prepare("call up_listar_Estudiante(?)");
+			$tempIdEstudiante = $estudiante->__GET('id_persona')->__GET('id_persona');
+			$statement->bindParam(1,$tempIdEstudiante);
+			$statement->execute();
+
+			foreach($statement->fetchAll(PDO::FETCH_OBJ) as $r)
+			{
+				$estudiante = new Estudiante();
+
+				$estudiante->__GET('id_persona')->__SET('id_persona', $r->id_persona);
+				$estudiante->__SET('codigo_estudiante', $r->codigo_estudiante);
+
+				$result[] = $estudiante;
+			}
+
+			return $result;
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+}
+
+?>
