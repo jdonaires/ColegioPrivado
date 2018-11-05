f8c21ddc8b27100f6331119fd4313cfbced620f7
farojastasayco <fabiolarojastasayco@gmail.com>
2018-11-05 01:51:54 -0500

    tipo_documentoDAO
    archivo DAO de la tabla tipo de documento

---
+++ PROYECTO/DAO/tipo_documentoDAO.php
@@ -0,0 +1,41 @@
+<?php
+require_once('../DAL/DBAccess.php');
+require_once('../BOL/tipo_documento.php');
+
+class Tipo_documentoDAO
+{
+    private $pdo;
+
+	public function __construct()
+	{
+			$dba = new DBAccess();
+			$this->pdo = $dba->get_connection();
+	}
+    public function Listar(Tipo_documento $tipo_documento)
+    {
+        try
+        {
+            $result = array();
+
+            $statement = $this->pdo->prepare("call up_listar_tipos_documentos(?)");
+            $statement->bindParam(1,$tipo_documento->__GET('id'));
+            $statement->execute();
+
+            foreach($statement->fetchAll(PDO::FETCH_OBJ) as $r)
+            {
+                $tipo_documento = new Tipo_documento();
+                $tipo_documento->__SET('id', $r->id_tdocumento);
+                $tipo_documento->__SET('tipo_documento', $r->tipo_documento);
+
+                $result[] = $tipo_documento;
+            }
+
+            return $result;
+        }
+        catch(Exception $e)
+        {
+            die($e->getMessage());
+        }
+    }
+}
+?>
