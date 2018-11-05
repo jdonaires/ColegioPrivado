acd46f0d0f8dd601877e5fba3ad923a87dada808
farojastasayco <fabiolarojastasayco@gmail.com>
2018-11-05 01:29:20 -0500

    Depuracion
    Cambios por los procesos de aula y docente

--- PROYECTO/BOL/docente.php
+++ PROYECTO/BOL/docente.php
@@ -1,22 +1,31 @@
 <?php
-require_once 'persona.php';
+
+require 'persona.php';
+require 'funcion.php';
 
 class Docente
 {
-  private $id_persona;
+	/*CAMPOS QUE REQUIERE LA TABLA DOCENTE*/
+	private $id_persona;
+ 	/*CAMPOS DE LA TABLA DOCENTE*/
 	private $estado;
+	/*CAMPOS QUE REQUIERE LA TABLA DOCENTE*/
 	private $id_funcion;
 
-	public function __CONSTRUCT()
+	/*CREANDO EL CONSTRUCTOR*/
+
+	public function __construct()
 	{
 		$this->id_persona = new Persona();
+		$this->id_funcion = new Funcion1();
 	}
+
+	/*EL GET (INGRESAR) Y EL SET (OBTENER)*/
 
 	public function __GET($x)
 	{
 		return $this->$x;
 	}
-
 	public function __SET($x, $y)
 	{
 		return $this->$x = $y;
