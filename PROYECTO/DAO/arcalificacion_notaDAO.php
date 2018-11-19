b490245236ca94111b0ed787f628eb0a58c44fcb
FJGutierrezP <42759084+FJGutierrezP@users.noreply.github.com>
2018-11-19 18:35:22 -0500

    correcion

--- PROYECTO/DAO/arcalificacion_notaDAO.php
+++ PROYECTO/DAO/arcalificacion_notaDAO.php
@@ -18,8 +18,8 @@ class Arcalificacion_notaDAO
 		{
 			$statement = $this->pdo->prepare("CALL up_registrar_arcalificacion_nota(?,?,?)");
 	    $statement->bindParam(1, $arcalificacion_nota->__GET('id_arcnota'));
-			$statement->bindParam(2, $arcalificacion_nota->__GET('id_arcalificacion'));
-			$statement->bindParam(3, $arcalificacion_nota->__GET('id_nota'));
+			$statement->bindParam(2, $arcalificacion_nota->__GET('id_arcalificacion')->__GET('id_arcalificacion'));
+			$statement->bindParam(3, $arcalificacion_nota->__GET('id_nota')->__GET('id_nota'));
 	    $statement -> execute();
 		} catch (Exception $e)
 		{
@@ -32,18 +32,16 @@ class Arcalificacion_notaDAO
 		try
 		{
 			$result = array();
-			$statement = $this->pdo->prepare("CALL up_buscar_arcalificacion_nota(?,?,?)");
+			$statement = $this->pdo->prepare("CALL up_buscar_arcalificacion_nota(?)");
 			$statement->bindParam(1, $arcalificacion_nota->__GET('id_arcnotas'));
-			$statement->bindParam(2, $arcalificacion_nota->__GET('id_arcalificacion'))
-			$statement->bindParam(3, $arcalificacion_nota->__GET('id_nota'))
 			$statement->execute();
 
 			foreach($statement->fetchAll(PDO::FETCH_OBJ) as $r)
 			{
 				$arcalificacion_nota = new Arcalificacion_nota();
 				$arcalificacion_nota->__SET('id_arcnota', $r->id_arcnota);
-				$arcalificacion_nota->__SET('id_arcalificacion', $r->id_arcalificacion);
-				$arcalificacion_nota->__SET('id_nota', $r->id_nota);
+				$arcalificacion_nota->__GET('id_arcalificacion')->__SET('id_arcalificacion', $r->id_arcalificacion);
+				$arcalificacion_nota->__GET('id_nota')->__SET('id_nota', $r->id_nota);
 
 				$result[] = $arcalificacion_nota;
 			}
